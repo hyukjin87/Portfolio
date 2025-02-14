011e151c258fd49fa943c8f8362e0b290504ca55
Hyuk Jin Kwon <wkwkdaus79@gmail.com>
2025-02-14 17:22:35 +0900

    update webpage

--- assets/js/jin.js
+++ assets/js/jin.js
@@ -7,3 +7,40 @@ function downloadResume() {
     link.click();
     document.body.removeChild(link);
   }
+
+
+  function toggleBubble(element) {
+    var bubble = element.parentElement.querySelector(".bubble");
+    bubble.style.display = "block";
+
+    // disappear 3 sec
+    setTimeout(function () {
+        bubble.style.display = "none";
+    }, 3000);
+}
+
+function startImageRotation(elementId, imageList, interval) {
+  let index = 0;
+  const element = document.getElementById(elementId);
+  
+  setInterval(() => {
+      index = (index + 1) % imageList.length;
+      element.src = imageList[index];
+  }, interval);
+}
+
+// def images
+const serverImages = [
+  "assets/imgs/ss_hilow_1.PNG",
+  "assets/imgs/ss_hilow_2.PNG",
+  "assets/imgs/ss_hilow_3.PNG"
+];
+const radioImages = [
+  "assets/imgs/ss_radio_1.PNG",
+  "assets/imgs/ss_radio_2.PNG",
+  "assets/imgs/ss_radio_3.PNG"
+];
+
+// call the common function
+startImageRotation("serverImage", serverImages, 1000);
+startImageRotation("radioImage", radioImages, 1000);
